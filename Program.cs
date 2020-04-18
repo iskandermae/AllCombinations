06f8e7d97dd4afee3891c3ba10d48060cd41ae4a
iskandermae <to_alexandr@ukr.net>
2020-04-18 23:10:46 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,8 +7,22 @@ namespace recurs
         static void Main(string[] args)
         {
             var cur = new int[4];
-            while (NextCombination(cur, 4)){
+            while (NextCombination(cur, 4))
+            {
                 Console.WriteLine(string.Join(' ', cur));
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("http://zonakoda.ru/generaciya-sochetanij.html");
+            int N = 4;
+            for (int M = 1; M <= N; M++)
+            {
+                int[] arr = null;
+                //int[] res;
+                while (GenerateCombinations(arr, M, N, out arr))
+                {
+                    Console.WriteLine(string.Join(' ', arr));
+                }
             }
         }
 
@@ -25,5 +39,28 @@ namespace recurs
                 }
             return false;
         }
+
+
+        private static bool GenerateCombinations(int[] arr, int M, int N,  out int[] res)
+        {
+            if (arr == null)
+            {
+                res = new int[M];
+                for (int i = 0; i < M; i++)
+                    res[i] = i + 1;
+                return true;
+            }
+            res = arr;
+            for (int i = M - 1; i >= 0; i--)
+                if (arr[i] < N - M + i + 1)
+                {
+                    arr[i]++;
+                    for (int j = i; j < M - 1; j++)
+                        arr[j + 1] = arr[j] + 1;
+                    return true;
+                }
+            return false;
+        }
     }
 }
+
